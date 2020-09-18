1012f6efa6eb5b7bc489a9d8601b31983d43cd61
David <david.kirchhoff@icloud.com>
2020-09-18 09:25:43 +0200

    Added tensor input to model

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -17,12 +17,15 @@ int main()
     Size s = Size((int) 640, (int) 360);
     int frame_num = 10;
     // File containing the object detection model being imported via torchscript (*.pt)
-    const char* model_file = "/Users/David/Repositories/traffic_object_detector/cpp/build/fasterrcnn50fpn.pt";
+    const char* model_file = "/Users/David/Repositories/traffic_object_detector/cpp/build/model/fasterrcnn50fpn.pt";
     // Import object detection model model from .pt file
-    torch::jit::script::Module model;
+    torch::jit::script::Module module;
     
     try {
-        model = torch::jit::load(model_file);
+        torch::jit::getProfilingMode() = false;   
+        torch::jit::getExecutorMode() = false;
+        torch::jit::setGraphExecutorOptimize(false);
+        module = torch::jit::load(model_file);
         std::cout << "Model import success!\n";
     }
     catch (const c10::Error& e) {
@@ -59,8 +62,6 @@ int main()
     }
 
 
-    
-
     // Loop through each frame
     int counter = 0;
     for (;;)
@@ -78,12 +79,29 @@ int main()
         if (counter % frame_num == 0)
         {
             cout << "Frame number..." << endl;
+       
+            Mat frame_rgb;
+            // Swap axis from BGR to RGB
+            cvtColor(frame, frame_rgb, COLOR_BGR2RGB); // Convert bgr to rgb
+            // Convert to tensor and add batch dimension
+            at::Tensor tensor_image = torch::from_blob(frame_rgb.data, {1, 3, frame_rgb.rows, frame_rgb.cols}, at::kByte);
+            cout << "tensor_image = " << endl << " " << tensor_image << endl << endl;
+            // normalize
+              
+            // Execute the model and turn its output into a tensor.
+            auto output = module.forward(tensor_image).toTensor();
+            Mat output_mat(cv::Size(640,360), CV_8UC3, output.data<float>());
+
+            Mat output8, output_bgr;
+            cvtColor(output8, output_bgr, COLOR_RGB2BGR);
 
 
-            /*
-            Bounding box prediction
-            */
-        // normalize()
+            //Mat output_mat({1920, 1080}, CV_8UC3, output.data<uint8>());
+            
+            
+            // Bounding box prediction
+            
+        
 
 
 
@@ -94,8 +112,6 @@ int main()
 
             counter = 0;
         }
-        cout << "No image" << endl;
-
         counter++;
 
         if (waitKey(5) >= 0)
